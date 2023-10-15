6ac53a2045bd0c515f0e549d02a9852dbc8ad09c
Uniekm <kvandijk@flemingcollege.ca>
2023-10-15 14:47:05 -0400

    ham menu

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
+    // Your existing mobile menu toggle code
     $('#mobile-menu-icon').click(function () {
         $('#nav-list').toggleClass('active');
-        $(this).toggleClass('toggle'); // Toggling a new class for animating the icon
+        $(this).toggleClass('toggle'); // Add this line
     });
 
+    // Initialize the particles effect
     particlesJS.load('particles-js', 'trippyconfig.json', function() {
         console.log('particles.js loaded');
-    });
+     });
 });
