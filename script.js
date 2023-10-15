add6a1412027b37971e5652dc8a808f92f02b46e
Uniekm <kvandijk@flemingcollege.ca>
2023-10-14 21:52:20 -0400

    big ups

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 $(document).ready(function () {
+    // Your existing mobile menu toggle code
     $('#mobile-menu-icon').click(function () {
         $('#nav-list').toggleClass('active');
     });
+
+    // Initialize the particles effect
+    particlesJS.load('particles-js', 'trippyconfig.json', function() {
+        console.log('particles.js loaded');
+     });
+     
 });
